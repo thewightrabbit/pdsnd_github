6ba1de03d49d6711ea95374ec6c94e4c531b24f1
thewightrabbit <101794284+thewightrabbit@users.noreply.github.com>
2022-03-30 18:42:11 +1100

    amended greeting in prompt

--- bikeshare.py
+++ bikeshare.py
@@ -37,7 +37,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
 
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hi! Let\'s explore some US bikeshare data!')
 
     # gets user input for city (chicago, new york city, washington) using a while loop to handle invalid inputs
     while True:
